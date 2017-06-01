87cf569c4881cf5d525921f49cae09bf8356f5d1
barinov3d <barinov3d@gmail.com>
2017-06-01 15:43:30 +0300

    Adding floating action button

--- app/src/main/java/com/barinov3dgmail/bill/MainActivity.java
+++ app/src/main/java/com/barinov3dgmail/bill/MainActivity.java
@@ -1,14 +1,44 @@
 package com.barinov3dgmail.bill;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab_plus;
+    Animation fabOpen, fabClose, fabRClocwise, fabRAntiClocwise;
+    boolean isOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab_plus = (FloatingActionButton) findViewById(R.id.fab_plus);
+        fabOpen = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fabClose = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+        fabRClocwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_clockwise);
+        fabRAntiClocwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_anticlockwise);
+
+        fab_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isOpen){
+                    fab_plus.startAnimation(fabRAntiClocwise);
+                    isOpen = false;
+
+                }
+                else
+                {
+                    fab_plus.startAnimation(fabRClocwise);
+                    isOpen = true;
+                }
+            }
+        });
     }
 }
